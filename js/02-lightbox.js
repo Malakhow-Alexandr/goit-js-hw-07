4b734c1c8d341a10c42ecd9b0c19757399b6a560
Malakhow-Alexandr <chuk3pc@gmail.com>
2022-12-16 14:07:55 +0200

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const imgMarkup = createMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createMarkup(items) {
+  return items
+    .map((item) => {
+      return `
+        <a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" data-source="${item.original}"  src="${item.preview}" alt="${item.description}" />
+        </a>
+      `;
+    })
+    .join("");
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
