29cd52b2f09be3616491c8bbd73f3940b9302d51
Malakhow-Alexandr <chuk3pc@gmail.com>
2022-12-16 12:38:39 +0200

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,7 +15,7 @@ function createMarkup(items) {
     .map((item) => {
       return `<div class="gallery__item">
         <a class="gallery__link" href="${item.original}">
-          <img class="gallery__image" data-pictureRef="${item.original}" data-pictureDesc="${item.description}" src="${item.preview}" alt="${item.description}" />
+          <img class="gallery__image" data-source="${item.original}" data-pictureDesc="${item.description}" src="${item.preview}" alt="${item.description}" />
         </a>
       </div>`;
     })
@@ -26,8 +26,19 @@ function onImgClick(evt) {
   if (!evt.target.classList.contains("gallery__image")) {
     return;
   }
+  evt.preventDefault();
+
+  window.addEventListener("keydown", onEscBtnPress);
+
   const instance = basicLightbox.create(
-    `<img src="${evt.target.dataset.pictureref}" alt="${evt.target.dataset.pictureDesc}" />`
+    `<img src="${evt.target.dataset.source}" alt="${evt.target.dataset.picturedesc}" />`
   );
   instance.show();
+
+  function onEscBtnPress(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscBtnPress);
+    }
+  }
 }
